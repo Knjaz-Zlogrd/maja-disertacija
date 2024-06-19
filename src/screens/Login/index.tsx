cf2db9abdc9ae6935163de9fd5aaed7a2288ce9e
Knjaz-Zlogrd <mulic92zr@gmail.com>
2024-06-19 17:26:20 +0200

    Add logic to show error and loading upon login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { loginWithEmailAndPassword, registerWithEmailAndPassword } from '../../auth';
 import { addAuthToken, addUID, setError, setLoading } from '../../store/loginSlice';
-import { useAppDispatch } from '../../store';
+import { useAppDispatch, useAppSelector } from '../../store';
 import { useNavigate } from 'react-router-dom';
 
 type LoginScreenProps = {
@@ -15,6 +15,9 @@ const Login = ({onLogin}: LoginScreenProps) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isRegistering, setIsRegistering] = useState(false);
+
+  const error = useAppSelector((state) => state.loginSlice.error);
+  const loading = useAppSelector((state) => state.loginSlice.loading);
 
 
   // const handleLogin = async (e: React.FormEvent) => {
@@ -48,17 +51,45 @@ const Login = ({onLogin}: LoginScreenProps) => {
     }
   };
 
+  // const handleRegister = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   try {
+  //     const user = await registerWithEmailAndPassword(email, password);
+  //     console.log('User registered:', user);
+  //     onLogin(email);
+
+  //   } catch (error) {
+  //     console.error('Error registering:', error);
+  //   }
+  // };
+
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+    dispatch(setLoading(true));
     try {
       const user = await registerWithEmailAndPassword(email, password);
-      console.log('User registered:', user);
-      onLogin(email);
+      const token = await user.getIdToken();
+      const uid = user.uid;
 
+      dispatch(addUID(uid));
+      dispatch(addAuthToken(token));
+      dispatch(setLoading(false));
+      onLogin(email);
+      navigate('/home');
     } catch (error) {
       console.error('Error registering:', error);
+      dispatch(setError('Registration failed. Please try again.'));
+      dispatch(setLoading(false));
     }
   };
+
+  const handleIsRegistering = () => {
+    setIsRegistering(!isRegistering);
+    dispatch(setLoading(false));
+    dispatch(setError(''));
+    setEmail('')
+    setPassword('');
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 via-pink-500 to-purple-500">
@@ -74,7 +105,7 @@ const Login = ({onLogin}: LoginScreenProps) => {
             <input
               type="email"
               id="email"
-              placeholder='Type your username'
+              placeholder="Type your username"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -88,17 +119,20 @@ const Login = ({onLogin}: LoginScreenProps) => {
             <input
               type="password"
               id="password"
-              placeholder='Type your password'
+              placeholder="Type your password"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
+          {loading && <div className="text-center text-blue-500 mb-4">Loading...</div>}
+          {error && !loading && <div className="text-center text-red-500 mb-4">{error}</div>}
           <div className="flex items-center justify-center mb-6">
             <button
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full focus:outline-none focus:shadow-outline"
+              disabled={loading}
             >
               {isRegistering ? 'Register' : 'Login'}
             </button>
@@ -107,7 +141,7 @@ const Login = ({onLogin}: LoginScreenProps) => {
             <a
               href="#"
               className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              onClick={() => setIsRegistering(!isRegistering)}
+              onClick={handleIsRegistering}
             >
               {isRegistering ? 'Already have an account? Login' : 'Don\'t have an account? Register'}
             </a>
